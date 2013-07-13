c3701a03d7248c099a608be9cdfb3d38bfa70f48
Bismark <bismark64@gmail.com>
2013-07-13 18:13:46 -0300

    some fixes

--- lib/delete.rb
+++ lib/delete.rb
@@ -7,8 +7,7 @@ module Checkmate
     unless files.nil?
       unless files.empty?
         files.each do |file|
-          File.delete(file) 
-          print_message green(I18n.t("delete.success.deleted_file", file: file))
+          delete_file(file)
         end
       else
         print_message yellow(I18n.t("delete.errors.no_file"))
@@ -17,6 +16,15 @@ module Checkmate
   end
 
   private
+
+  def self.delete_file
+    begin
+      File.delete(file) 
+      print_message green(I18n.t("delete.success.deleted_file", file: file))
+    rescue Errno::EACCES
+      print_message red(I18n.t("delete.errors.permission_denied"))
+    end
+  end
 
   def self.print_message(message)
     puts message
